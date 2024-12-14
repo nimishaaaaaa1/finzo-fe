409d0301173c4bcbbe6b85efc1ead48825685080
nimishaaaaaa1 <nimichanda932@gmail.com>
2024-12-14 15:11:33 +0530

    domain changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,12 +18,35 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-[#F2F2F2] text-[#211A1F]`}> {/* Updated background color */}
+      <head>
+        {/* Google Tag Manager */}
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-K8N5VS88');
+          `
+        }} />
+        {/* End Google Tag Manager */}
+      </head>
+      <body className={`${inter.className} bg-[#F2F2F2] text-[#211A1F]`}>
         <Header />
         <main className="min-h-screen pt-16">
           {children}
         </main>
         <Footer />
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-K8N5VS88"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
       </body>
     </html>
   )
